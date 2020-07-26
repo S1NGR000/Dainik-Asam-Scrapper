dc24c133ea0919a7f7202bfa1859bf621dcc906b
S1NGR000 <s1ngr000@s1ngr000.xyz>
2020-07-26 10:28:24 +0530

    first commit

---
+++ scrapper.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import wget
+import datetime
+import os
+
+today = datetime.datetime.today().strftime ('%d-%b-%Y')
+#today = str(today)
+#print(today)
+
+source1 = requests.get('http://www.assamtribune.com/da/index.html').text
+soup1 = BeautifulSoup(source1, 'lxml')
+
+for meta1 in soup1.find_all('meta'):
+    meta = meta1['content']
+
+link1 = re.sub('0; url=', '', meta)
+
+source2 = requests.get(link1).text
+soup2 = BeautifulSoup(source2, 'lxml')
+
+link2 = soup2.find(class_= 'Toplink')['href']
+link2 = link2.replace("javascript:PopupWindow('atda.asp?id", "http://www.assamtribune.com/scripts/atda.asp?id")
+link2 = link2.replace("');", "")
+
+source3 = requests.get(link2).text
+soup3 = BeautifulSoup(source3, 'lxml')
+link3 = soup3.find(id="pdfdownload")['href']
+
+wget.download(link3, out=f'Dainik_Asam_{today}.pdf')
